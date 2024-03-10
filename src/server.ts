399dce1e5dc153193da1dfbd4ca08992e16483bd
shivanshu877 <vashudev8777@gmail.com>
2024-03-10 23:28:43 +0530

    endpoint's added

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express' 
 import router from './router' 
+import morgan from 'morgan'
 const app = express()
-
+app.use(morgan('dev')) //It can be used to log requests, errors, and more to the console    
+app.use(express.json()) //It can be used to parse incoming request bodies in a middleware before your handlers, available under the req.body property.
+app.use(express.urlencoded({extended: true})) //It can be used to parse incoming request bodies in a middleware before your handlers, available under the req.body property.    
 app.get('/' ,(req , res) => {
 console.log("hello")
 res.status(200)
