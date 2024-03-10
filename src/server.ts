a4b748fe34605f030b87ea592a563043e1979399
shivanshu877 <vashudev8777@gmail.com>
2024-03-10 23:16:55 +0530

    update updatepoint  schema added

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
-import express from 'express'
-
+import express from 'express' 
+import router from './router' 
 const app = express()
 
 app.get('/' ,(req , res) => {
-console.log("Sdf")
+console.log("hello")
 res.status(200)
 res.json({message: 'hello'});
 });
 
-export default app
+app.use('/api', router)
+export default app 
