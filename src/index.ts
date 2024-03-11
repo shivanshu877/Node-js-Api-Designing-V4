d52bdbb862cfee16a8e57763e2eef05f845d578e
shivanshu877 <vashudev8777@gmail.com>
2024-03-11 23:49:58 +0530

    jwt added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import app  from './server'
-
+import * as dotenv from 'dotenv'
+dotenv.config() // import  all env variables from env file to but prisma will able to access database url because it have something else for searching
+// we added this in our  entry file then it will be available to all the files in our project
 app.listen(3001, () => {    
     console.log('Server is running on port 3001');
  });
---
+++ src/modules/auth.ts
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken'
+
+
+export const createJWT = (user) =>{
+const token = jwt.sign({id : user.id , username : user.username}, process.env.JWT_SECRET, )
+
+return token
+}
+
+
+export const protect =(req , res) =>{
+     const bearer = req.headers.authorization
+     if(!bearer){
+        res.status(401).json({message: 'You are not authorized'})
+    return
+     }
+}
