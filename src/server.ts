30937cffa79a1b2320680c60d22c5d365144fb9b
shivanshu877 <vashudev8777@gmail.com>
2024-03-11 23:12:20 +0530

    cors added

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express' 
 import router from './router' 
 import morgan from 'morgan'
+import cors from 'cors' //  restricted resources on a web page to be requested from another domain outside the domain from which the first resource was served.
 const app = express()
+app.use(cors()) //It can be used to enable CORS with various options.
 app.use(morgan('dev')) //It can be used to log requests, errors, and more to the console    
 app.use(express.json()) //It can be used to parse incoming request bodies in a middleware before your handlers, available under the req.body property.
 app.use(express.urlencoded({extended: true})) //It can be used to parse incoming request bodies in a middleware before your handlers, available under the req.body property.    
