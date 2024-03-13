93f8182e114b8d10693294e322d98f84cb3fabad
shivanshu877 <vashudev8777@gmail.com>
2024-03-13 23:02:56 +0530

    user create and sign in added

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import router from './router'
 import morgan from 'morgan'
 import cors from 'cors' //  restricted resources on a web page to be requested from another domain outside the domain from which the first resource was served.
 import { protect } from './modules/auth'
+import { create } from 'domain'
+import { createNewUser, signIn } from './handlers/user'
 const app = express()
 app.use(cors()) //It can be used to enable CORS with various options.
 app.use(morgan('dev')) //It can be used to log requests, errors, and more to the console    
@@ -13,6 +15,7 @@ console.log("hello")
 res.status(200)
 res.json({message: 'hello'});
 });
-
+app.post('/user'  , createNewUser)
+app.post('/signin'  , signIn)
 app.use('/api', protect   ,  router)
 export default app 
