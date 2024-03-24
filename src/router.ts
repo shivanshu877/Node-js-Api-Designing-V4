7a7d857bc7564c8871958fe4334e37b053c8f37e
shivanshu877 <vashudev8777@gmail.com>
2024-03-24 19:09:23 +0530

    product handlers added

--- src/router.ts
+++ src/router.ts
@@ -1,115 +1,106 @@
 import { Router } from 'express';
 import { body, param, validationResult } from 'express-validator';
 import handleInputErrors from './middlewares';
+import { CreateNewProduct, GetAllProducts, GetaOneProduct, UpdateProduct, DeleteProduct } from '../src/handlers/product';
 const router = Router();
 
 
-router.get('/product', (req: Request, res: any) => {
-    res.json({ message: 'hello' });
-    res.status(200)
-});
+router.get('/product', GetAllProducts)
+
+router.get('/product/:id', GetaOneProduct)
 
 
-router.get('/product/:id', (req: Request, res: any) => {
+router.put('/product/:id', body('name').isString(), handleInputErrors, UpdateProduct)
 
 
-});
+router.post('/product', body('name').isString().not().isEmpty(), handleInputErrors, CreateNewProduct);
 
 
-router.put('/product/:id', body('name').isString(), handleInputErrors, (req: Request, res: any) => {
 
-    return res.status(200).json({ msg: 'good' })
-
-
-}
-);
-
-
-router.post('/product', body('name').isString(), handleInputErrors, (req, res) => {
-
-    return res.status(200).json({ msg: 'good' })
-});
-
-
-router.delete('/product/:id', param('id').exists().toInt(), handleInputErrors, (req, res) => {
-    return res.status(200).json({ msg: 'good' })
-
-});
+router.delete('/product/:id', DeleteProduct)
 
 
 
 
 
 
-router.get('/update', (req, res) => {
+router.get('/update', (req: Request, res: any) => {
 
 
 });
 
 
-router.get('/update/:id', param('id').exists().toInt(), body('title').optional().isString(),
-    body('title').optional().isString(), body('body').optional().isString(),
-    body('status').matches(/^(IN_PROGRESS | SHIPPED | DEPRECATED)$/).withMessage('Status must be either accept, reject, or declined'),
-    handleInputErrors, (req, res) => {
+router.get('/update/:id', param('id').exists().toInt().isInt({ min: 0 }), handleInputErrors, (req: Request, res: any) => {
+
+
+});
+
+
+router.put('/update/:id', param('id').exists().toInt(), (req: Request, res: any) => {
+
+
+});
+
+
+router.post('/update',
+    body('title').exists().isString(),
+    body('body').exists().isString(),
+    body('productId').exists().isString(),
+    handleInputErrors, (req: Request, res: any) => {
+
+        return res.status(200).json({ msg: 'good' })
+
+    });
+
+
+router.delete('/update/:id', (req: Request, res: any) => {
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+router.get('/updatepoint', (req: Request, res: any) => {
+
+
+});
+
+
+router.get('/updatepoint/:id', (req: Request, res: any) => {
+
+
+});
+
+
+router.put('/updatepoint/:id', (req: Request, res: any) => {
+
+
+});
+
+
+router.post('/updatepoint',
+
+    body('name').exists().isString(),
+    body('desciripton').exists().isString(),
+    body('updateID').exists().isString(),
+    handleInputErrors,
+    (req: Request, res: any) => {
 
 
     });
 
 
-router.put('/update/:id', param('id').exists().toInt(), (req, res) => {
-
-
-});
-
-
-router.post('/update', (req, res) => {
-
-
-});
-
-
-router.delete('/update/:id', (req, res) => {
-
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-router.get('/updatepoint', (req, res) => {
-
-
-});
-
-
-router.get('/updatepoint/:id', (req, res) => {
-
-
-});
-
-
-router.put('/updatepoint/:id', (req, res) => {
-
-
-});
-
-
-router.post('/updatepoint', (req, res) => {
-
-
-});
-
-
-router.delete('/updatepoint/:id', (req, res) => {
+router.delete('/updatepoint/:id', (req: Request, res: any) => {
 
 
 });
