f3545a1debcf8761630950a9d5754a50fd7a628d
Shivanshu <shivanshu@ivypods.com>
2024-09-07 16:08:44 +0530

    done  for today

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,23 @@
-import express from 'express' 
-import router from './router' 
-import morgan from 'morgan'
-import cors from 'cors' //  restricted resources on a web page to be requested from another domain outside the domain from which the first resource was served.
-import { protect } from './modules/auth'
-import { create } from 'domain'
-import { createNewUser, signIn } from './handlers/user'
-const app = express()
-app.use(cors()) //It can be used to enable CORS with various options.
-app.use(morgan('dev')) //It can be used to log requests, errors, and more to the console    
-app.use(express.json()) //It can be used to parse incoming request bodies in a middleware before your handlers, available under the req.body property.
-app.use(express.urlencoded({extended: true})) //It can be used to parse incoming request bodies in a middleware before your handlers, available under the req.body property.    
-app.get('/'  , (req , res) => {
-console.log("hello")
-res.status(200)
-res.json({message: 'hello'});
-});
-app.post('/user'  , createNewUser)
-app.post('/signin'  , signIn)
-app.use('/api', protect   ,  router)
-export default app 
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import authRoutes from './routes/auth';
+import userRoutes from './routes/users';
+import projectRoutes from './routes/projects';
+import auditLogRoutes from './routes/auditLogs';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev')) 
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+app.use('/projects', projectRoutes);
+app.use('/audit-logs', auditLogRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
